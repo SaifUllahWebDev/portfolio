7121e070335c2d5b06b08213fd431153668c3e1b
SaifUllahWebDev <saifullahwebdev@gmail.com>
2025-01-24 18:07:31 -1100

    project section
    the buttons are now working

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 var front = document.getElementsByClassName("front");
-var back = document.getElementsByClassName("back");
+var back = document.getElementsByClassName("backs");
 
-
-function frontend(){
-    
-     back.style.display = "none"
+function frontend() {
+    // Show front-end projects, hide back-end projects
+    for (var i = 0; i < front.length; i++) {
+        front[i].style.display = "block";
+    }
+    for (var i = 0; i < back.length; i++) {
+        back[i].style.display = "none";
+    }
 }
 
-function backend(){
-    front.style.display= "none";
+function backend() {
+    // Show back-end projects, hide front-end projects
+    for (var i = 0; i < front.length; i++) {
+        front[i].style.display = "none";
+    }
+    for (var i = 0; i < back.length; i++) {
+        back[i].style.display = "block";
+    }
 }
 
-function all (){
-    back.style.display = "block"
-    front.style.display= "block";
+function all() {
+    // Show all projects
+    for (var i = 0; i < front.length; i++) {
+        front[i].style.display = "block";
+    }
+    for (var i = 0; i < back.length; i++) {
+        back[i].style.display = "block";
+    }
 }
